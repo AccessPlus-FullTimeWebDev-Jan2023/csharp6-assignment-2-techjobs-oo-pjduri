486b0c1ed8b7e07fa47c8d5c1aec40887275ee55
Your Name <108307350+pjduri@users.noreply.github.com>
2023-04-17 11:12:47 -0500

    re-commented runtechjobs

--- TechJobsOOAutoGraded6/RunTechJobs.cs
+++ TechJobsOOAutoGraded6/RunTechJobs.cs
@@ -10,19 +10,19 @@ namespace TechJobsOOAutoGraded6
         {
             // TODO: Task 4: Remove this line to uncomment the RunProgram method once you complete the Job class
 
-                Job job1 = Samples.job1;
-                Job job2 = Samples.job2;
-                Job job3 = Samples.job3;
+                //Job job1 = Samples.job1;
+                //Job job2 = Samples.job2;
+                //Job job3 = Samples.job3;
 
-                List<Job> jobs = new List<Job>();
-                    jobs.Add(job1);
-                    jobs.Add(job2);
-                    jobs.Add(job3);
+                //List<Job> jobs = new List<Job>();
+                //    jobs.Add(job1);
+                //    jobs.Add(job2);
+                //    jobs.Add(job3);
 
-                foreach (Job job in jobs)
-                {
-                    Console.WriteLine(job);
-                }
+                //foreach (Job job in jobs)
+                //{
+                //    Console.WriteLine(job);
+                //}
 
             // TODO: Task 4: Remove this line to uncomment the RunProgram method once you complete the Job class */
 
